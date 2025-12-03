de254cdeabb7f330af4709c886bd811adaf190ff
Nguyen Tri Cuong <nguyentricuong1504@gmail.com>
2025-12-03 09:58:16 +0700

    max nguoofn mo

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ app.use(morgan('combined')) // xử dụng để xem log của resquest HTTP
 app.use(bodyParser.urlencoded({ extended: false })) // Sử dụng body-parser để phân tích cú pháp URL-encoded trong các yêu cầu đến
 app.use(bodyParser.json()) // Sử dụng body-parser để phân tích cú pháp JSON trong các yêu cầu đến
 
-app.use('/branches', branchRouter);
+// app.use('/branches', branchRouter);
+app.use("/",(req, res)=>{
+    res.send("Branch Service is up and running hehe");
+});
 
 
 // app.listen(3002, () => {
